8439c19eb8719c4773c497bef714f91fe3607582
Tyler Cosgrove <tylercosgrove42@gmail.com>
2023-01-21 18:29:25 -0500

    Send note on Home

--- src/Components/NewNote.js
+++ src/Components/NewNote.js
@@ -4,13 +4,19 @@ import {useState, useEffect, useRef, useContext} from "react";
 import '../Styles/BigNote.css';
 import { RelayContext,UserContext } from "../Extras/UserContext";
 import useAutosizeTextArea from "../Extras/useAutosizeTextArea";
+import * as elliptic from 'elliptic';
+import { sha256 } from "js-sha256";
+import * as secp from '@noble/secp256k1';
 
 
 const NewNote = () => {
 
+    const {relays} = useContext(RelayContext);
     const {user, setUser} = useContext(UserContext);
     const [value, setValue] = useState("");
     const textAreaRef = useRef(null);
+    const ec = new elliptic.ec('secp256k1');
+
 
     useAutosizeTextArea(textAreaRef.current, value);
 
@@ -41,6 +47,48 @@ const NewNote = () => {
       }
     }
 
+    const publish = async () => {
+        if(value=="") {
+            return;
+        }
+        setValue("");
+        const keypair = ec.keyFromPrivate(user.privkey, 'hex');
+
+        var event = {
+            "content"    : value,
+            "created_at" : Math.floor( Date.now() / 1000 ),
+            "kind"       : 1,
+            "tags"       : [],
+            "pubkey"     : user.pubkey,
+        }
+
+        var eventData = JSON.stringify([
+            0,
+            event['pubkey'],
+            event['created_at'],
+            event['kind'],
+            event['tags'],
+            event['content']
+        ])
+
+        event.id  = sha256( eventData ).toString( 'hex' );
+        const signature2 = await secp.schnorr.sign(event.id, user.privkey);
+        event.sig = secp.utils.bytesToHex(signature2);
+
+        relays.forEach(relay => {
+            sendEvent(relay, event);
+        });
+    }
+
+    const sendEvent = (socket, eventData) => {
+        const ws = new WebSocket(socket);
+        ws.onopen = (event) => {
+            console.log(eventData);
+            ws.send(JSON.stringify(["EVENT",eventData]));
+            ws.close();
+        }
+    }
+
 
     return (
         <>
@@ -55,7 +103,7 @@ const NewNote = () => {
                 <p>{getName()}</p>
                 <textarea id="new-note" onChange={handleChange} placeholder="What's happening?" rows={1} value={value} ref={textAreaRef}/>
 
-                <button id="publish-button" class="classic-button">Publish</button>
+                <button id="publish-button" class="classic-button" onClick={publish}>Publish</button>
               </div>
             </div>
           </>}
